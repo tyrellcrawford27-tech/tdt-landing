88fe6a74e6af4890da5cf05f8a2043006567b2b4
Tyrell Crawford <Tyrell@Blacks-MacBook-Pro.local>
2026-06-30 18:06:32 -0400

    Fix sticky scroll broken by overflow-x-hidden on html element

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full antialiased overflow-x-hidden">
+    <html lang="en" className="h-full antialiased">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
